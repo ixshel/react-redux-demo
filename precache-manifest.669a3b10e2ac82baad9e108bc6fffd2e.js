539405b0d9bc60065192b9182ff3fce265a86d4b
ixshel escamilla <ixshelescamilla@gmail.com>
2019-01-22 11:33:52 -0800

    Updates

---
+++ precache-manifest.669a3b10e2ac82baad9e108bc6fffd2e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-redux-demo/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "f8d71995e147c14f5741",
+    "url": "/react-redux-demo/static/js/runtime~main.f8d71995.js"
+  },
+  {
+    "revision": "b54f559104bcbc22f1cb",
+    "url": "/react-redux-demo/static/js/main.b54f5591.chunk.js"
+  },
+  {
+    "revision": "698640b00dd779024f93",
+    "url": "/react-redux-demo/static/js/1.698640b0.chunk.js"
+  },
+  {
+    "revision": "b54f559104bcbc22f1cb",
+    "url": "/react-redux-demo/static/css/main.5d753bc0.chunk.css"
+  },
+  {
+    "revision": "64ad4fba90f255917efda9906c4ec74d",
+    "url": "/react-redux-demo/index.html"
+  }
+];
